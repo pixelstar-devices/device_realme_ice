2f83c5a1972b70987407f57db04e85d8fc8a4b07
abhay4721 <abhayabhi4721@gmail.com>
2024-05-06 15:44:27 +0530

    Say welcome to Pixelstar
    bringup for pixelstar

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_ice.mk
+    $(LOCAL_DIR)/pixelstar_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_ice-user \
-    aosp_ice-userdebug \
-    aosp_ice-eng
+    pixelstar_ice-user \
+    pixelstar_ice-userdebug \
+    pixelstar_ice-eng
--- pixelstar_ice.mk
+++ pixelstar_ice.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ice device
 $(call inherit-product, device/realme/ice/device.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
 
 # Environment Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -22,7 +22,7 @@ TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 #Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_ice
+PRODUCT_NAME := pixelstar_ice
 PRODUCT_DEVICE := ice
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
